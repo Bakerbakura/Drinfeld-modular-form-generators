9915b62ba2db7c0689b90b6836caa7b1779cbff2
Dr LB Baker <liambaker@hpc1.hpc>
2021-06-10 21:55:19 +0200

    Added q,deg-specific bash scripts

---
+++ 2,3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# load SageMath
+cd ~/Drinfeld-modular-form-generators
+module load app/SageMath/9.2
+
+# run the computation
+sage cuspExpansions.sage 2 3
+
+# email that the computation is complete
+mail -s "q=2, deg=3 computation complete" liambaker@sun.ac.za
