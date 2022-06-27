b8f540f9dc08ca8dfc8a3888e777007725fcf118
Dr Liam Baker <liambaker@sun.ac.za>
2022-06-27 12:57:52 +0200

    Added initial versions of hypothetical running shell scripts

---
+++ h2,4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# load SageMath
+cd ~/Drinfeld-modular-form-generators
+module load app/SageMath/9.2
+
+# run the computation
+sage cuspExpansions.sage 2 4 hypo
+
+# email that the computation is complete
+echo "" | mail -s "q=2, deg=4 hypothetical computation complete" liambaker@sun.ac.za
---
+++ h4,2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# load SageMath
+cd ~/Drinfeld-modular-form-generators
+module load app/SageMath/9.2
+
+# run the computation
+sage cuspExpansions.sage 4 2 hypo
+
+# email that the computation is complete
+echo "" | mail -s "q=4, deg=2 hypothetical computation complete" liambaker@sun.ac.za
